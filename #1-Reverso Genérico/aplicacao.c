76d79d3be5a73a5795a1bfa1f4a6bc1f7d5310a9
Gabriel Tomio <tomio.gabriel@outlook.com>
2023-04-10 19:15:52 -0300

    + First code, generic reverse. + It is already working, but it doesn't follow the desired structure.

---
+++ #1-Reverso Genérico/aplicacao.c
@@ -0,0 +1,139 @@
+/*******************************************************************************
+* Title                 :   aplicacao  
+* Filename              :   aplicacao.c
+* Author                :   Gabriel Tomio
+* Origin Date           :   01/04/2023
+* Version               :   1.0.0
+* Compiler              :   GCC
+* Notes                 :   Reverse generic input
+*
+*******************************************************************************/
+/******************************************************************************
+* Includes
+*******************************************************************************/
+#include "aplicacao.h"
+
+/******************************************************************************
+* Module Typedefs
+*******************************************************************************/
+
+/******************************************************************************
+* Module Variable Definitions
+*******************************************************************************/
+
+/******************************************************************************
+* Function Prototypes
+*******************************************************************************/
+void * store_data(void * p_memory, void * p_data, int data_size);
+
+/******************************************************************************
+* Function Definitions
+*******************************************************************************/
+// TODO: UPDATE AND COPY THESE FOR EACH NON_TRIVIAL FUNCTION
+/** ****************************************************************************
+* Function : Dio_Init()
+* @description: This function is used to initialize the Dio based on the configuration table
+*  defined in dio_cfg module.  
+*
+* @return 		
+*
+*******************************************************************************/
+int main(int argc, char **argv)
+{
+    char input_text[10];
+    char * input_type;
+
+    char line[MAXIMUM_SIZE_OF_LINE];
+
+    char * p_new_value;
+    void * p_stored_data;
+
+    var_type_t data_type;
+    int data_type_size;
+    
+    int num_of_data = 0;
+
+    p_stored_data = malloc(MAX_FULL_DATA_SIZE_BYTES);
+    // p_new_value = malloc(MAX_SINGLE_VALUE_SIZE_BYTES);
+
+    // printf("p_stored_data: %p; p_new_value: %p\n", p_stored_data, p_new_value);
+
+    // if(!fscanf(stdin, "%s %s", input_text, input_type))
+    if(fgets(line, MAXIMUM_SIZE_OF_LINE, stdin) == NULL)
+    {
+        printf("Invalid Input File.");
+        return FINISHED_ERROR;
+    }
+
+    input_type = strtok(line, DELIMITER_CHARACTER);
+    // input_type = strtok(NULL, DELIMITER_CHARACTER);
+    
+    // printf("type_text %s\n", input_type);
+
+    
+    data_type = get_data_type(input_type);
+    data_type_size = get_type_size(data_type);
+
+    // printf("input_text: %s\n", line);
+    if(data_type_size == -1)
+    {
+        printf("input_text: %s\n", line);
+        // printf("Invalid Data Type Error!\n");
+        return FINISHED_ERROR;
+    }
+
+    /* fgets(A, B, C) */ 
+    /* A => Where it will store the received data */ 
+    /* B => Maximum Size of the Line, it will stop in this position if line has more than this size */ 
+    /* C => Pointer to the file that is open by fopen or in this case, passed as a parameter */ 
+    while (fgets(line, MAXIMUM_SIZE_OF_LINE, stdin) != NULL)
+    {
+        int temp_int;
+        char temp_char;
+        void * temp_pointer;
+
+        p_new_value = strtok(line, DELIMITER_CHARACTER);
+
+        while ((p_new_value != NULL) && (strcmp(p_new_value, "\n") != 0))
+        {
+            num_of_data ++;
+            // printf("new: %c\n", ((char*)p_new_value)[0]);
+
+            if(data_type == INT_TYPE)
+            {
+                temp_int = atoi(p_new_value);
+                temp_pointer = (void *) &temp_int;
+            }
+            else
+            {
+                temp_char = p_new_value[0];
+                temp_pointer = (void *) &temp_char;
+            }
+
+            // printf("before -> temp_char: %c; temp_pointer: %p \n", ((char *)(temp_pointer))[0], temp_pointer);
+            p_stored_data = store_data(p_stored_data, temp_pointer, data_type_size);
+
+            p_new_value = strtok(NULL, DELIMITER_CHARACTER); // In this case we put NULL so it continues in the same line and on the next delimiter
+        }
+    }
+
+    p_stored_data = p_stored_data - num_of_data*data_type_size; // Restore pointer to its initial value
+
+    // printf("input -> p_stored_data: %p\n", p_stored_data);
+
+    print_reverse_generic(p_stored_data, data_type, num_of_data);
+
+    free(p_stored_data);
+
+    return FINISHED_SUCCESS;
+}
+
+void * store_data(void * p_memory, void * p_data, int data_size)
+{
+    for(int i = 0; i < data_size; i++)
+    {
+        ((char *)p_memory)[i] = ((char *)p_data)[i];
+    }
+    return (void *)p_memory + data_size;
+}
+/*************** END OF FUNCTIONS ***************************************************************************/
---
+++ #1-Reverso Genérico/rgen.c
@@ -0,0 +1,124 @@
+/*******************************************************************************
+* Title                 :   aplicacao  
+* Filename              :   aplicacao.c
+* Author                :   Gabriel Tomio
+* Origin Date           :   01/04/2023
+* Version               :   1.0.0
+* Compiler              :   GCC
+* Notes                 :   Reverse generic input
+*
+*******************************************************************************/
+/** @file aplicacao.c
+ *  @brief This is the source file for TODO: WHAT DO I DO? 
+ */
+/******************************************************************************
+* Includes
+*******************************************************************************/
+#include "rgen_publico.h"
+#include "rgen_privado.h"
+
+/******************************************************************************
+* Module Preprocessor Constants
+*******************************************************************************/
+
+
+/******************************************************************************
+* Module Preprocessor Macros
+*******************************************************************************/
+
+/******************************************************************************
+* Module Typedefs
+*******************************************************************************/
+
+/******************************************************************************
+* Module Variable Definitions
+*******************************************************************************/
+
+/******************************************************************************
+* Function Prototypes
+*******************************************************************************/
+
+/******************************************************************************
+* Function Definitions
+*******************************************************************************/
+
+void print_reverse_generic(void * p_data, var_type_t data_type, int num_of_data)
+{   
+    print_data_type(data_type);
+
+    for(int i = (num_of_data - 1); i >= 0; i--)
+    {
+        if(data_type == INT_TYPE)
+        {
+            int number = ((int*)p_data)[i];
+            int remainder;
+            // int reverse = 0;
+            printf("(");
+            while (number != 0) 
+            {   
+                //todo: when the value is 1000 it is printing 1 instead of 0001
+                remainder = number % 10;
+                // reverse = reverse * 10 + remainder;
+                number /= 10;
+                printf("%d", remainder);
+            }
+            printf(");\n");
+        }
+        else
+        {
+            printf("(%c);\n",((char*)p_data)[i]);
+        }
+    }
+}
+
+void print_data_type(var_type_t data_type)
+{
+    printf("Tipo_do_dado:");
+    if(data_type == INT_TYPE)
+    {
+        printf("int;\n");
+    }
+    else if(data_type == CHAR_TYPE)
+    {
+        printf("char;\n");
+    }
+    else
+    {
+        printf("none;\n");
+    }
+}
+
+int get_type_size(var_type_t var_type)
+{
+    switch (var_type)
+    {
+        case INT_TYPE:
+            return (sizeof(int));
+        break;
+
+        case CHAR_TYPE:
+            return (sizeof(char));
+        break;
+        
+        default:
+            printf("get_type_size() -> Invalid Type\n");
+            return -1;
+        break;
+    }
+}
+
+var_type_t get_data_type(char var_type_text[])
+{
+    if(strcmp(var_type_text, "Tipo:int") == 0)
+    {
+        return INT_TYPE;
+    }
+    else if (strcmp(var_type_text, "Tipo:char") == 0)
+    {
+        return CHAR_TYPE;
+    }
+
+    printf("get_data_type() -> Invalid Type\n");
+    return ERROR_TYPE;
+}
+/*************** END OF FUNCTIONS ***************************************************************************/
